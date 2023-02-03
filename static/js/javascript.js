0d67b6b4d965a1180f770b243619db07004d9faa
FamiLeeAhihi <ellylee0811@gmail.com>
2023-02-03 11:39:20 +0700

    init

---
+++ static/js/javascript.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        nav: false,
+        responsive:{
+            0:{
+                items:1
+            }
+        }
+    })
+});
